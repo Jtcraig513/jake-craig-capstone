d9f115bcc830143e3628ba14c2f993a9dc9473dd
Jake Craig <jtcraig513@gmail.com>
2024-04-10 01:06:51 -0400

    Got the add/remove collection button to work. Most troublesome part so far

--- src/components/CollectionMovie/CollectionMovie.js
+++ src/components/CollectionMovie/CollectionMovie.js
@@ -1,25 +1,48 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './CollectionMovie.scss';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function CollectionMovie({ movie }) {
 
-    const [isRemoved, setIsRemoved] = useState(false);
+    const [isRemoved, setIsRemoved] = useState(null);
 
-    const handleClick = (event) => {
-        if (event.target.innerHTML == '+ Collection'){
-            event.target.innerHTML = '&#10003; Collection';
-            setIsRemoved(false);
-        } else {
-            event.target.innerHTML = '+ Collection';
-            setIsRemoved(true);
+    // useEffect(() => {
+    //     if (isRemoved) {
+    //         // If movie is marked as removed, update the button text
+    //         event.target.innerHTML = '&#10003; Collection';
+    //     } else {
+    //         // Otherwise, set the button text to default
+    //         event.target.innerHTML = '+ Collection';
+    //     }
+    // }, [isRemoved]);
+
+    const handleClick = () => {
+        // Toggle the removed state
+        setIsRemoved(prevIsRemoved => !prevIsRemoved);
+    };
+
+    useEffect(() => {
+        // Only perform API call if there is a change in the removed state
+        if (isRemoved!=null) {
+            // Send a PUT request to update the movie's removed status
+            axios.put(`${SERVER_URL}/collection/${movie.movie_id}`, {
+                movieId: movie.movie_id,
+                removed: isRemoved // Toggle the removed flag
+            }, { withCredentials: true })
+            .then(() => {
+                console.log(`Movie ${movie.movie_id} updated successfully.`);
+            })
+            .catch((error) => {
+                console.error('Error updating movie:', error);
+            });
         }
-        return;
-    }
+    }, [isRemoved]);
     
     return (
-        <section className="movie">
+        <section className="movie" id={`${movie.movie_id}`}>
             <Link to={`/movie/${movie.movie_id}`} className='movie__link'>
                 <img className='movie__poster' src={movie.poster} alt={`${movie.title} Poster`} />
             </Link>
@@ -29,7 +52,7 @@ function CollectionMovie({ movie }) {
                     <p className='p-large movie__buncher-title'>{movie.title}</p>
                 </Link>
             
-                <button className='movie__buncher-button' onClick={handleClick}>&#10003; Collection</button>
+                <button className='movie__buncher-button' onClick={handleClick}>{!isRemoved ? '\u2713 Collection' : '+ Collection'}</button>
             </section>
         </section>
     )
